ec575f0adc59e1451c45ccb1f33d5375e454243d
Светайло Данил ИН-01 <danil.svetaylo@gmail.com>
2025-11-27 15:44:39 +0200

    Add transport map

---
+++ Areas/Admin/Controllers/TransportController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TraineeApplication.Model;
+
+namespace SumyCRM.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class TransportController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(new LoginViewModel());
+        }
+
+    }
+}
