92ca2ddd4574ca5241732322460ee55a7574e2c9
Светайло Данил ИН-01 <danil.svetaylo@gmail.com>
2025-12-01 13:47:35 +0200

    Add caller field

--- Services/CategoryConverter.cs
+++ Services/CategoryConverter.cs
@@ -5,6 +5,7 @@
         public static readonly Dictionary<string, Guid> MenuToCategory = new()
         {
             ["1.1"] = Guid.Parse("5ef8838e-3264-4277-9604-92b004d97224"), // Вода
+
         };
 
     }
