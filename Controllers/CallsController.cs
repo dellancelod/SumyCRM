9c4596300978fa90378617aac62dbf4599cb7d4d
Светайло Данил ИН-01 <danil.svetaylo@gmail.com>
2025-12-02 09:11:30 +0200

    Fix api controller

--- Controllers/CallsController.cs
+++ Controllers/CallsController.cs
@@ -2,19 +2,20 @@
 using Microsoft.AspNetCore.Mvc;
 using OpenAI.Audio;
 using SumyCRM.Data;
+using SumyCRM.Models;
 using static SumyCRM.Services.CategoryConverter;
 
 namespace SumyCRM.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-    public class ApiController : Controller
+    public class CallsController : Controller
     {
         private readonly string _apiKey;
         private readonly DataManager _dataManager;
         private readonly IConfiguration _config;
 
-        public ApiController(DataManager dataManager, IConfiguration config)
+        public CallsController(DataManager dataManager, IConfiguration config)
         {
             _config = config;
             _dataManager = dataManager;
@@ -89,72 +90,6 @@ namespace SumyCRM.Controllers
 
             return Ok("Uploaded");
         }
-        [HttpPost("/api/upload")]
-        [AllowAnonymous]
-        [IgnoreAntiforgeryToken] // for curl
-        public async Task<IActionResult> Upload(IFormFile audio,
-           string caller, string menu_item,
-           [FromHeader(Name = "X-API-KEY")] string apiKey,
-           [FromServices] IConfiguration config)
-        {
-            string secret = config["UploadSecret"];
-            if (apiKey != secret)
-                return Unauthorized("Invalid API Key");
-
-            if (audio == null || audio.Length == 0)
-                return BadRequest("No audio file");
-
-            string folder = Path.Combine("wwwroot", "audio");
-
-            Directory.CreateDirectory(folder);
-
-            string fileName = $"{Guid.NewGuid()}_{audio.FileName}";
-            string fullPath = Path.Combine(folder, fileName);
-            using (var stream = new FileStream(fullPath, FileMode.Create))
-            {
-                await audio.CopyToAsync(stream);
-            }
-
-            // ===== Whisper STT =====
-
-            AudioClient audioClient = new("whisper-1", _apiKey);
-            AudioTranscriptionOptions options = new()
-            {
-                // Force Ukrainian
-                Language = "uk",
-
-                // можно не задавать, по умолчанию вернётся просто текст
-                ResponseFormat = AudioTranscriptionFormat.Text
-            };
-            AudioTranscription transcription =
-                     await audioClient.TranscribeAudioAsync(fullPath, options);
-            string transcript = transcription.Text ?? "(empty)";
-
-
-            // ===== Save in DB =====
-            if (!MenuToCategory.TryGetValue(menu_item, out var categoryId))
-                return BadRequest("Unknown menu item");
-            MenuToText.TryGetValue(menu_item, out var menu_text);
-
-            var category = await _dataManager.Categories.GetCategoryByIdAsync(categoryId);
-            if (category == null)
-                return BadRequest("Category not found");
-
-            var record = new Request
-            {
-                RequestNumber = _dataManager.Requests.GetRequests().Count() + 1,
-                CategoryId = category.Id,
-                Category = category,
-                Caller = caller,
-                Text = menu_text,
-                Address = transcript,
-                IsCompleted = false,
-                AudioFilePath = "/audio/" + fileName
-            };
-
-            await _dataManager.Requests.SaveRequestAsync(record);
-
-            return Ok("Uploaded");
-        }
+        
     }
 }
--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -2,7 +2,6 @@ using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using SumyCRM.Data;
 using SumyCRM.Models;
-using static SumyCRM.Services.CategoryConverter;
 using Microsoft.AspNetCore.Mvc;
 using OpenAI;
 using OpenAI.Chat;
@@ -14,8 +13,6 @@ using TraineeApplication.Model;
 
 namespace SumyCRM.Controllers
 {
-    [ApiController]
-    [Route("api/[controller]")]
     public class HomeController : Controller
     {
         private readonly UserManager<IdentityUser> userManager;
@@ -66,74 +63,7 @@ namespace SumyCRM.Controllers
             await signInManager.SignOutAsync();
             return RedirectToAction("Index", "Home");
         }
-        [HttpPost("/api/upload")]
-        [AllowAnonymous]
-        [IgnoreAntiforgeryToken] // for curl
-        public async Task<IActionResult> Upload(IFormFile audio,
-           string caller, string menu_item,
-           [FromHeader(Name = "X-API-KEY")] string apiKey,
-           [FromServices] IConfiguration config)
-        {
-            string secret = config["UploadSecret"];
-            if (apiKey != secret)
-                return Unauthorized("Invalid API Key");
-
-            if (audio == null || audio.Length == 0)
-                return BadRequest("No audio file");
-
-            string folder = Path.Combine("wwwroot", "audio");
-
-            Directory.CreateDirectory(folder);
-
-            string fileName = $"{Guid.NewGuid()}_{audio.FileName}";
-            string fullPath = Path.Combine(folder, fileName);
-            using (var stream = new FileStream(fullPath, FileMode.Create))
-            {
-                await audio.CopyToAsync(stream);
-            }
-
-            // ===== Whisper STT =====
-
-            AudioClient audioClient = new("whisper-1", _apiKey);
-            AudioTranscriptionOptions options = new()
-            {
-                // Force Ukrainian
-                Language = "uk",
-
-                // можно не задавать, по умолчанию вернётся просто текст
-                ResponseFormat = AudioTranscriptionFormat.Text
-            };
-            AudioTranscription transcription =
-                     await audioClient.TranscribeAudioAsync(fullPath, options);
-            string transcript = transcription.Text ?? "(empty)";
-
-            
-            // ===== Save in DB =====
-            if (!MenuToCategory.TryGetValue(menu_item, out var categoryId))
-                return BadRequest("Unknown menu item");
-
-            MenuToText.TryGetValue(menu_item, out var menu_text);
-
-            var category = await _dataManager.Categories.GetCategoryByIdAsync(categoryId);
-            if (category == null)
-                return BadRequest("Category not found");
-
-            var record = new Request
-            {
-                RequestNumber = _dataManager.Requests.GetRequests().Count() + 1,
-                CategoryId = category.Id,
-                Category = category,
-                Caller = caller,
-                Text = menu_text,
-                Address = transcript,
-                IsCompleted = false,
-                AudioFilePath = "/audio/" + fileName
-            };
-
-            await _dataManager.Requests.SaveRequestAsync(record);
-
-            return Ok("Uploaded");
-        }
+       
     }
 
 }
